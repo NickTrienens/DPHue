aecc57ec7c21f7f51d30fa771f31a5e0c4d71d3f
Jason Dreisbach <jtdreisb@gmail.com>
2013-05-12 19:54:02 -0700

    Make a DPhue singleton object

--- DPHue/DPHue.h
+++ DPHue/DPHue.h
@@ -6,6 +6,29 @@
 //
 //
 
+#import <Foundation/Foundation.h>
 #import <DPHue/DPHueBridge.h>
-#import <DPHue/DPHueDiscover.h>
 #import <DPHue/DPHueLight.h>
+#import <DPHue/DPHueDiscover.h>
+
+@protocol DPHueDelegate <NSObject>
+@optional
+- (void)alertUserToPressLinkButtonOnBridge:(DPHueBridge *)bridge;
+@end
+
+@interface DPHue : NSObject <DPHueDiscoverDelegate>
+
+@property (readonly) BOOL isSearching;
+@property (readonly) NSArray *bridges;
+@property (readonly) NSArray *lights;
+
+@property (weak) id <DPHueDelegate> delegate;
+
++ (id)sharedInstance;
+
+- (void)startDiscovery;
+- (void)cancelDiscovery;
+
+- (void)reloadBridgeData;
+
+@end
---
+++ DPHue/DPHue.m
@@ -0,0 +1,139 @@
+//
+//  DPHue.m
+//  Pods
+//
+//  Created by Jason Dreisbach on 5/2/13.
+//
+//
+
+#import "DPHue.h"
+
+NSString *const kHueUsernamePrefKey = @"HueAPIUsernamePrefKey";
+
+
+@interface DPHue ()
+{
+    DPHueDiscover *_dhd;
+    NSMutableArray *_bridges;
+}
+
+@property (readwrite) BOOL isSearching;
+
+@end
+
+@implementation DPHue
+
++ (id)sharedInstance
+{
+    static DPHue *sharedHueInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedHueInstance = [[DPHue alloc] init];
+    });
+    return sharedHueInstance;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _bridges = [NSMutableArray array];
+        _isSearching = NO;
+        
+        NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+        if ([prefs objectForKey:kHueUsernamePrefKey] == nil) {
+            NSString *username = [DPHueBridge generateUsername];
+            [prefs setObject:username forKey:kHueUsernamePrefKey];
+            [prefs synchronize];
+        }
+    }
+    return self;
+}
+
+- (void)startDiscovery
+{
+    if (_dhd == nil)
+        _dhd = [[DPHueDiscover alloc] initWithDelegate:self];
+    
+    if (_isSearching == NO) {
+        self.isSearching = YES;
+        [_dhd discoverForDuration:0 withCompletion:^(NSMutableString *log) {
+            [self willChangeValueForKey:@"isSearching"];
+            self.isSearching = NO;
+            [self didChangeValueForKey:@"isSearching"];
+        }];
+    }
+}
+
+- (void)cancelDiscovery
+{
+    [_dhd stopDiscovery];
+    self.isSearching = NO;
+    _dhd = nil;
+}
+
+- (NSArray *)bridges
+{
+    return _bridges;
+}
+
+- (NSArray *)lights
+{
+    NSMutableArray *lightArray = [NSMutableArray array];
+    for (DPHueBridge *bridge in _bridges) {
+        [lightArray addObjectsFromArray:bridge.lights];
+    }
+    return lightArray;
+}
+
++ (NSSet *)keyPathsForValuesAffectingLights
+{
+    return [NSSet setWithArray:@[@"bridges"]];
+}
+
+- (void)reloadBridgeData
+{
+    [self willChangeValueForKey:@"bridges"];
+    for (DPHueBridge *bridge in _bridges) {
+        [bridge readWithCompletion:^(DPHueBridge *hue, NSError *err) {
+            if (err != nil) {
+                NSLog(@"%s: %@", __PRETTY_FUNCTION__, err);
+            }
+        }];
+    }
+    [self didChangeValueForKey:@"bridges"];
+}
+
+#pragma mark - DPHueDiscover delegate
+
+- (void)foundHueAt:(NSString *)host discoveryLog:(NSMutableString *)log {
+    for (DPHueBridge *existingBridge in _bridges) {
+        if ([existingBridge.host isEqualToString:host])
+            return;
+    }
+    DPHueBridge *newHue = [[DPHueBridge alloc] initWithHueHost:host username:[[NSUserDefaults standardUserDefaults] objectForKey:kHueUsernamePrefKey]];
+    
+    [newHue readWithCompletion:^(DPHueBridge *hue, NSError *err) {
+        if (err == nil) {
+            if (hue.authenticated == YES) {
+                [self willChangeValueForKey:@"bridges"];
+                [_bridges addObject:hue];
+                [self didChangeValueForKey:@"bridges"];
+            }
+            else {
+                [hue registerUsername];
+                if ([_delegate respondsToSelector:@selector(alertUserToPressLinkButtonOnBridge:)]) {
+                    [_delegate alertUserToPressLinkButtonOnBridge:hue];
+                }
+                else {
+                    NSLog(@"Found Hue. Press the bridge button to complete registration!");
+                }
+            }
+        }
+        else {
+            NSLog(@"Could not read hue: %@", err);
+        }
+    }];
+}
+
+@end
